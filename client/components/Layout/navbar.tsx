ea1504c86facf6488d6448f61d30f5ba0bea32c8
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-02-23 15:49:23 +0900

    add

--- client/components/Layout/navbar.tsx
+++ client/components/Layout/navbar.tsx
@@ -1,33 +1,64 @@
 import { Layout, Menu, Row, Col } from "antd";
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+
+import type { MenuProps } from "antd";
+const items: MenuProps["items"] = [
+  {
+    label: "Navigation One",
+    key: "mail",
+  },
+
+  {
+    label: "Navigation Three ",
+    key: "SubMenu",
+    children: [
+      {
+        type: "group",
+        label: "Item 1",
+        children: [
+          {
+            label: "Option 1",
+            key: "setting:1",
+          },
+          {
+            label: "Option 2",
+            key: "setting:2",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: (
+      <a href="" target="_blank" rel="noopener noreferrer">
+        Navigation Four - Link
+      </a>
+    ),
+    key: "alipay",
+  },
+];
 const Navbar: React.FC = () => (
   <Row align="middle">
     <Col
       style={{ textAlign: "left" }}
-      xl={{ span: 2, offset: 2 }}
-      lg={{ span: 2, offset: 2 }}
+      xl={{ span: 2, offset: 4 }}
+      lg={{ span: 2, offset: 4 }}
       sm={{ span: 4, offset: 10 }}
       xs={{ span: 4, offset: 10 }}
     >
       <a href="">비행기</a>
     </Col>
     <Col
-      xl={{ span: 4, offset: 10 }}
-      lg={{ span: 4, offset: 10 }}
+      xl={{ span: 8, offset: 6 }}
+      lg={{ span: 8, offset: 6 }}
       sm={{ offset: 0, span: 0 }}
       xs={{ offset: 0, span: 0 }}
     >
-      <Menu mode="horizontal" style={{ backgroundColor: "transparent" }}>
-        <Menu.Item key="mail">
-          <a href="">비행기</a>
-        </Menu.Item>
-        <Menu.Item key="app">
-          <a href="">구매</a>
-        </Menu.Item>
-        <Menu.Item>
-          <a href="">Mypage</a>
-        </Menu.Item>
-      </Menu>
+      <Menu
+        mode="horizontal"
+        style={{ backgroundColor: "transparent" }}
+        items={items}
+      ></Menu>
     </Col>
     <Col
       xl={{ span: 0, offset: 0 }}
@@ -39,8 +70,8 @@ const Navbar: React.FC = () => (
       <a href="">메뉴</a>
     </Col>
     <Col
-      xl={{ span: 2, offset: 4 }}
-      lg={{ span: 2, offset: 4 }}
+      xl={{ span: 2, offset: 2 }}
+      lg={{ span: 2, offset: 2 }}
       sm={{ span: 0, offset: 2 }}
       xs={{ span: 0, offset: 2 }}
       style={{ textAlign: "right" }}
