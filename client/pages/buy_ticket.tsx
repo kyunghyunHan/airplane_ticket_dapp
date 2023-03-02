5a733db936690d5e89afb22bdeed410f6aa77750
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-03-02 23:09:46 +0900

    add

--- client/pages/buy_ticket.tsx
+++ client/pages/buy_ticket.tsx
@@ -1,6 +1,45 @@
-import { Inter } from "@next/font/google";
+import { useEffect, useState } from "react";
+
+export const moduleAddress =
+  "0xa604279e6129beb5fa225673daa13f0fa87095e9a576687d1924120a7777b2be";
+export const NODE_URL = "https://fullnode.testnet.aptoslabs.com";
+import { AptosClient } from "aptos";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+export const client = new AptosClient(NODE_URL);
 
 export default function Reservation() {
+  const { account, signAndSubmitTransaction } = useWallet();
+  const [newTask, setNewTask] = useState<any>("");
+  console.log(account);
+  useEffect(() => {
+    const genRandomKey = async () => {
+      if (!account) return [];
+      const todoListResource = await client.getAccountResource(
+        account?.address,
+        `${moduleAddress}::tickets::Flight`
+      );
+      console.log(todoListResource);
+      const taskCounter = (todoListResource as any).data.counter;
+      setNewTask(taskCounter);
+    };
+
+    genRandomKey();
+  }, [account]);
+
+  useEffect(() => {
+    const genRandomKey = async () => {
+      if (!account) return [];
+      const todoListResource = await client.getAccountResource(
+        account?.address,
+        `${moduleAddress}::tickets::AirplaneTicket`
+      );
+      console.log(todoListResource);
+      const taskCounter = (todoListResource as any).data.counter;
+      setNewTask(taskCounter);
+    };
+
+    genRandomKey();
+  }, [account]);
   return (
     <>
       <div>fafa</div>
