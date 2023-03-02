6d2e05ae3cb8c5bee26208cff4331bfc8dbc5394
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-03-02 11:07:38 +0900

    add

--- client/components/Layout/navbar.tsx
+++ client/components/Layout/navbar.tsx
@@ -1,20 +1,22 @@
 import {
   createStyles,
-  Menu,
   Center,
   Header,
   Container,
   Group,
-  Button,
   Burger,
+  ScrollArea,
+  Drawer,
+  Divider,
 } from "@mantine/core";
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+
 import { useDisclosure } from "@mantine/hooks";
 const HEADER_HEIGHT = 60;
 const useStyles = createStyles(theme => ({
   Header: {
     position: "fixed",
   },
-
   inner: {
     height: HEADER_HEIGHT,
     display: "flex",
@@ -23,11 +25,17 @@ const useStyles = createStyles(theme => ({
   },
 
   links: {
+    padding: "100px",
     [theme.fn.smallerThan("lg")]: {
       display: "none",
     },
   },
 
+  no: {
+    [theme.fn.smallerThan("lg")]: {
+      display: "none",
+    },
+  },
   burger: {
     [theme.fn.largerThan("lg")]: {
       display: "none",
@@ -54,14 +62,13 @@ const useStyles = createStyles(theme => ({
           : theme.colors.gray[0],
     },
   },
-
-  linkLabel: {
-    marginRight: 5,
-  },
-  acess: {
-    [theme.fn.smallerThan("lg")]: {
+  hiddenDesktop: {
+    [theme.fn.largerThan("sm")]: {
       display: "none",
     },
+  },
+  linkLabel: {
+    marginRight: 5,
   },
 }));
 interface HeaderActionProps {
@@ -72,48 +79,86 @@ interface HeaderActionProps {
 }
 
 const Navbar = ({ links }: HeaderActionProps) => {
-  const { classes } = useStyles();
-  const [opened, { toggle }] = useDisclosure(false);
+  const [opened, { toggle: toggleDrawer, close: closeDrawer }] =
+    useDisclosure(false);
+  const { classes, theme } = useStyles();
+
   const items = links.map(link => {
     return (
-      <Menu key={link.label} trigger="hover" exitTransitionDuration={0}>
-        <Menu.Target>
-          <a
-            href={link.link}
-            className={classes.link}
-            onClick={event => event.preventDefault()}
-          >
-            <Center>
-              <span className={classes.linkLabel}>{link.label}</span>
-            </Center>
-          </a>
-        </Menu.Target>
-      </Menu>
+      <a
+        href={link.link}
+        className={classes.link}
+        onClick={event => event.preventDefault()}
+      >
+        <Center>
+          <span className={classes.linkLabel}>{link.label}</span>
+        </Center>
+      </a>
     );
   });
   return (
-    <Header
-      className={classes.Header}
-      height={HEADER_HEIGHT}
-      sx={{ borderBottom: 0 }}
-      mb={10}
-    >
-      <Container className={classes.inner} fluid>
+    <>
+      <Header
+        className={classes.Header}
+        height={HEADER_HEIGHT}
+        sx={{ borderBottom: 0 }}
+        mb={10}
+      >
+        <Container className={classes.inner} fluid>
+          <Group>
+            <h1>LOGO</h1>
+          </Group>
+          <Group>
+            <Group spacing={5} className={classes.links}>
+              {items}
+            </Group>
+            <Group className={classes.no}>
+              <WalletSelector />
+            </Group>
+
+            <Burger
+              opened={opened}
+              onClick={toggleDrawer}
+              className={classes.burger}
+              size="sm"
+            />
+          </Group>
+        </Container>
+      </Header>
+      <Drawer
+        opened={opened}
+        onClose={closeDrawer}
+        overlayColor={
+          theme.colorScheme === "dark"
+            ? theme.colors.dark[9]
+            : theme.colors.gray[2]
+        }
+        overlayOpacity={0.55}
+        overlayBlur={3}
+        size="50%"
+        position="top"
+        padding="md"
+        className={classes.hiddenDesktop}
+        zIndex={1000000}
+        sx={{ textAlign: "center" }}
+      >
         <h1>LOGO</h1>
-        <Group spacing={4} className={classes.links}>
+        <ScrollArea sx={{ height: "calc(100vh - 60px)" }} mx="-md">
+          <Divider
+            my="sm"
+            color={theme.colorScheme === "dark" ? "dark.5" : "gray.1"}
+          />
           {items}
-        </Group>
-        <Button className={classes.acess} radius="xl" sx={{ height: 30 }}>
-          Get early access
-        </Button>
-        <Burger
-          opened={opened}
-          onClick={toggle}
-          className={classes.burger}
-          size="sm"
-        />
-      </Container>
-    </Header>
+          <Divider
+            my="sm"
+            color={theme.colorScheme === "dark" ? "dark.5" : "gray.1"}
+          />
+          <Group position="center" grow pb="xl" px="md">
+            <WalletSelector />
+          </Group>
+        </ScrollArea>
+      </Drawer>
+    </>
   );
 };
 
